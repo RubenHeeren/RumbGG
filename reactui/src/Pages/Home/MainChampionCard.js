4da77dfa22213f1ad4971a426a7c660037b0d1b9
Cullington <70056723+RubenHeeren@users.noreply.github.com>
2021-10-31 21:48:02 +0100

    Last fixes

--- reactui/src/Pages/Home/MainChampionCard.js
+++ reactui/src/Pages/Home/MainChampionCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { numberWithCommas } from "../../Utilities/UtilityFunctions";
+import { Constants } from "../../Utilities/Constants";
 
 export default function MainChampionCard(props) {
   return (
     <Card className="bg-extradark p-3">
-      <Card.Img variant="top" src={`https://localhost:7124/${props.top3PlayedChampionsCardDTO.relativeImagePath}`} />
+      <Card.Img variant="top" src={`${Constants.STATIC_FILE_URL_CHAMPION_TILES}/${props.top3PlayedChampionsCardDTO.name.replace(/[^a-zA-Z0-9]/g, '')}_0.jpg`} />
       <Card.Body className="pb-0">
         <Card.Title>{props.top3PlayedChampionsCardDTO.name}</Card.Title>
         <Card.Text>
