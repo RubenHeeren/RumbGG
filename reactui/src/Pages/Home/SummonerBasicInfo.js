a23a352b9deb825857c9e5a1d365b1c0e1fd3261
Cullington <70056723+RubenHeeren@users.noreply.github.com>
2021-10-31 21:26:01 +0100

    Attempt to fix opgg profile pic link didn't work

--- reactui/src/Pages/Home/SummonerBasicInfo.js
+++ reactui/src/Pages/Home/SummonerBasicInfo.js
@@ -21,6 +21,7 @@ export default function SummonerBasicInfo() {
             <Col md={2} xs={4}>
               <img
                 className="mw-100 mt-2"
+                // changed
                 src={`//opgg-static.akamaized.net/images/profile_icons/profileIcon${context.summonerState.summoner.profileIconId}.jpg?image=q_auto:best&v=1518361200`}
                 alt="Summoner profile icon"
               />
